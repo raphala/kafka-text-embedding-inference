cbe6263ffe7c4981b7a279c8896893346f9b487c
raphala <22345578+raphala@users.noreply.github.com>
2024-10-30 17:01:33 +0100

    Use ONNX runtime

--- inference-app/src/model.py
+++ inference-app/src/model.py
@@ -7,10 +7,8 @@ from main import BATCH_SIZE
 
 logging.basicConfig(level=logging.INFO)
 
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2', backend="onnx")
 
-
-# model = SentenceTransformer('intfloat/multilingual-e5-large-instruct')
 
 def get_embedding(input_text: str) -> Tensor:
     logging.info("encoding embedding for %s", input_text)
