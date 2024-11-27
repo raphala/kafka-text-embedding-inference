e59665fbbde3bc1d72656141d0d7b1378c00634f
raphala <22345578+raphala@users.noreply.github.com>
2024-11-27 19:47:12 +0100

    Switch to fastembed-fpu

--- inference-app/src/model.py
+++ inference-app/src/model.py
@@ -1,14 +1,15 @@
 import logging
 
-from sentence_transformers import SentenceTransformer
-from torch import Tensor
+import numpy as np
 
 from main import BATCH_SIZE
+from fastembed import TextEmbedding
 
 logging.basicConfig(level=logging.INFO)
 
-model = SentenceTransformer('sentence-transformers/msmarco-bert-base-dot-v5', backend="onnx")
+model = TextEmbedding(model_name="BAAI/bge-large-en-v1.5", providers=["CUDAExecutionProvider"])
 
 
-def get_embedding(input_text: list[str]) -> Tensor:
+def get_embedding(input_text: list[str]) -> list[np.ndarray]:
+    logging.info("encoding %i embeddings", len(input_text))
     return model.encode(input_text, batch_size=BATCH_SIZE)
