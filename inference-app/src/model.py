e8866ee6d48189ae66421d370f25ab8130fd4730
raphala <22345578+raphala@users.noreply.github.com>
2024-11-28 15:37:16 +0100

    Add GPU support

--- inference-app/src/model.py
+++ inference-app/src/model.py
@@ -4,9 +4,12 @@ from fastembed import TextEmbedding
 from config import EXECUTION_PROVIDER, EMBEDDING_MODEL
 from logger import logger
 
-logger.info("Loading embedding model %s with execution provider %s", EMBEDDING_MODEL, EXECUTION_PROVIDER)
-model = TextEmbedding(model_name=EMBEDDING_MODEL, providers=[EXECUTION_PROVIDER])
-
+try:
+    logger.info("Loading embedding model %s with execution provider %s", EMBEDDING_MODEL, EXECUTION_PROVIDER)
+    model = TextEmbedding(model_name=EMBEDDING_MODEL, providers=[EXECUTION_PROVIDER])
+    logger.info("Successfully loaded embedding model")
+except Exception as e:
+    logger.error("Error loading model", e)
 
 def get_embedding(input_text: list[str]) -> list[np.ndarray]:
     logger.info("Encoding %i embeddings", len(input_text))
