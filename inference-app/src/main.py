0590d18b55975f0e37774afc4163a2a44a6e9a10
raphala <22345578+raphala@users.noreply.github.com>
2024-11-29 17:51:44 +0100

    Clean up project

--- inference-app/src/main.py
+++ inference-app/src/main.py
@@ -7,7 +7,7 @@ from confluent_kafka.serialization import StringDeserializer
 
 import consumer
 from config import SCHEMA_REGISTRY, BOOTSTRAP_SERVER, OUTPUT_TOPIC, BATCH_SIZE, CHUNK_SIZE, CHUNK_OVERLAP, \
-    EXECUTION_PROVIDER
+    EXECUTION_PROVIDERS
 from embeddingproducer import EmbeddingProducer
 from logger import logger
 from paper import dict_to_paper
@@ -17,6 +17,7 @@ from paper import dict_to_paper
 # TODO prepare deployment
 # TODO avro/protobuf support
 # TODO autoscaling - scaling with transactional api?
+# TODO implement deserializing consumer that supports everything
 
 PROJECT_ROOT = Path(__file__).parent.parent.parent
 
@@ -32,7 +33,7 @@ def load_schema(schema_name: str) -> dict:
 
 if __name__ == '__main__':
     logger.info("Starting inference app with provider %s, chunk size %i, chunk overlap %i, batch size %i",
-                EXECUTION_PROVIDER, CHUNK_SIZE, CHUNK_OVERLAP, BATCH_SIZE)
+                EXECUTION_PROVIDERS, CHUNK_SIZE, CHUNK_OVERLAP, BATCH_SIZE)
     schema_registry_conf = {'url': SCHEMA_REGISTRY}
     schema_registry_client = SchemaRegistryClient(schema_registry_conf)
     string_deserializer = StringDeserializer('utf_8')
@@ -61,8 +62,3 @@ if __name__ == '__main__':
 
     producer = EmbeddingProducer(producer_config, qdrant_json_serializer, OUTPUT_TOPIC)
     consumer.run_consumer(consumer_config, producer, paper_json_deserializer)
-
-# TODO clean up env variables
-# TODO add switch gpu/cpu to env variables
-# TODO don't pass values around
-# TODO implement deserializing consumer that supports everything
